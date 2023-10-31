e9ee16d0dd7f1dd78600d08a359bea4a6bf1e864
Rostislav <52472843+elleroys@users.noreply.github.com>
2023-10-31 15:48:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,283 @@
+﻿using System.Threading.Tasks;
+
+namespace Modul
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string id;
+            int capasity;
+            int currentAmount;
+            JuiceType juice;
+            Tank tank = null;
+
+            Console.WriteLine("Passing tests....");
+
+            // ==================== Test 1 ====================
+            Console.WriteLine("\n===== Test 1: =====");
+            id = "";
+            capasity = 1000;
+
+            try
+            {
+                tank = new Tank(id, capasity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 2 ====================
+            Console.WriteLine("\n===== Test 2: =====");
+            id = "tank_00";
+            capasity = -100;
+
+            try
+            {
+                tank = new Tank(id, capasity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 3 ====================
+            Console.WriteLine("\n===== Test 3: =====");
+            id = "tank_00";
+            capasity = 0;
+
+            try
+            {
+                tank = new Tank(id, capasity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 4 ====================
+            Console.WriteLine("\n===== Test 4: =====");
+            id = "tank_00";
+            capasity = 1000;
+            currentAmount = -50;
+
+            try
+            {
+                tank = new Tank(id, capasity, currentAmount, JuiceType.Apple);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 5 ====================
+            Console.WriteLine("\n===== Test 5: =====");
+            id = "tank_00";
+            capasity = 1000;
+            currentAmount = 950;
+
+            try
+            {
+                tank = new Tank(id, capasity, currentAmount, JuiceType.Apple);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 6 ====================
+            Console.WriteLine("\n===== Test 6: =====");
+            id = "tank_00";
+            capasity = 1000;
+            currentAmount = 650;
+            juice = JuiceType.Unknown;
+
+            try
+            {
+                tank = new Tank(id, capasity, currentAmount, juice);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 7 ====================
+            Console.WriteLine("\n ===== Test 7: =====");
+            id = "tank_00";
+            capasity = 1000;
+            currentAmount = 650;
+            juice = JuiceType.Tomato;
+
+            try
+            {
+                tank = new Tank(id, capasity, currentAmount, juice);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 8 ====================
+            Console.WriteLine("\n===== Test 8: =====");
+            Console.WriteLine(tank.GetInfo());
+            Console.WriteLine($"Free amount: {tank.FreeAmount}");
+            Console.WriteLine($"Free? {tank.IsFree}");
+            Console.WriteLine($"Full? {tank.IsFull}");
+
+            // ==================== Test 9 ====================
+            Console.WriteLine("\n===== Test 9: =====");
+            bool result = tank.AddJuice(200, JuiceType.Tomato);
+            Console.WriteLine($"Add juice? {result}");
+
+            // ==================== Test 10 ====================
+            Console.WriteLine("\n===== Test 10: =====");
+            result = tank.AddJuice(250, JuiceType.Orange);
+            Console.WriteLine($"Add juice? {result}");
+            Console.WriteLine($"Capacity: {tank.Capacity}");
+            Console.WriteLine($"Current amount: {tank.CurrentAmount}");
+            Console.WriteLine($"Free amount: {tank.FreeAmount}");
+            Console.WriteLine($"Free? {tank.IsFree}");
+            Console.WriteLine($"Full? {tank.IsFull}");
+
+            // ==================== Test 11 ====================
+            Console.WriteLine("\n===== Test 11: =====");
+            result = tank.AddJuice(150, JuiceType.Orange);
+            Console.WriteLine($"Add juice? {result}");
+            Console.WriteLine($"Full? {tank.IsFull}");
+
+            // ==================== Test 12 ====================
+            Console.WriteLine("\n===== Test 12: =====");
+            tank.MakeFree();
+            Console.WriteLine($"Free? {tank.IsFree}");
+
+            // ==================== Test 13 ====================
+            Console.WriteLine("\n===== Test 13: =====");
+            id = "tank_01";
+            capasity = 1000;
+            currentAmount = 350;
+            juice = JuiceType.Tomato;
+            Tank t1 = new Tank(id, capasity, currentAmount, juice);
+
+            id = "tank_02";
+            capasity = 1000;
+            currentAmount = 450;
+            juice = JuiceType.Tomato;
+            Tank t2 = new Tank(id, capasity, currentAmount, juice);
+
+            PourJuice(t1, t2);
+
+            // ==================== Test 14 ====================
+            Console.WriteLine("\n===== Test 14: =====");
+            id = "tank_03";
+            capasity = 1000;
+            currentAmount = 150;
+            juice = JuiceType.Orange;
+            Tank t3 = new Tank(id, capasity, currentAmount, juice);
+
+            PourJuice(t1, t3);
+
+            // ==================== Test 15 ====================
+            Console.WriteLine("\n===== Test 15: =====");
+            id = "tank_04";
+            capasity = 1000;
+            currentAmount = 50;
+            juice = JuiceType.Apple;
+            Tank t4 = new Tank(id, capasity, currentAmount, juice);
+            PourJuice(t1, t4);
+
+            // ==================== Test 16 ====================
+            Console.WriteLine("\n===== Test 16: =====");
+            id = "tank_05";
+            capasity = 1000;
+            Tank t5 = new Tank(id, capasity);
+            PourJuice(t5, t3);
+            Console.WriteLine($"Tank count = {Tank.TankCount}");
+
+            // ==================== Test 17 ====================
+            Console.WriteLine("\n===== Test 17: =====");
+            // Cтворити колекцію зі всіх створених у Main() резервуарів (ШІСТЬ об'єктів класу Tank)
+
+            // your code!
+            List<Tank> tanks = new List<Tank> { t1, t2, t3, t4, t5 };
+
+            foreach (var t in tanks)
+            {
+                Console.WriteLine(t.GetInfo());
+            }
+            // Пройтись по колекції і для кожного резервуару вивести інформацію про нього 
+
+            // your code!
+
+            // ==================== Test 18 ====================
+            Console.WriteLine("\n===== Test 18: =====");
+            // Використовуючи методи класу List<T> знайти резервуари, в яких зберігається томатних сік
+
+            // your code!
+
+            var tomatoTanks = tanks.Where(t => t.Juice == JuiceType.Tomato);
+            foreach (var tomatoTank in tomatoTanks)
+            {
+                Console.WriteLine(tomatoTank.GetInfo());
+            }
+            // Вивести іноформацію про всі знайдені резервуари з томатним соком
+
+            // your code!
+
+            // ==================== Test 19 ====================
+            Console.WriteLine("\n===== Test 19: =====");
+            //Використовуючи методи і властивості класу List<T> вивести на екран кількість пустих резервуарів:
+
+            // your code!
+            int count = tanks.Count(t => t.IsFree);
+            Console.WriteLine($"Amount of free tanks = {count}");
+
+            // ==================== Test 20 ====================
+            Console.WriteLine("\n===== Test 20: =====");
+            //Використовуючи методи класу List<T> видалии із колекції всі пусті резервуари:
+
+            // your code!
+
+            // Вивести іноформацію про резервуари, які залишилися у колекції:
+
+            // your code!
+            tanks.RemoveAll(t => t.IsFree);
+
+            foreach (var t in tanks)
+            {
+                Console.WriteLine(t.GetInfo());
+            }
+        }
+
+        public static bool AllowedPour(Tank t1, Tank t2)
+        {
+            if (t1.Juice == t2.Juice && !t1.IsFull && !t2.IsFree)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void PourJuice(Tank t1, Tank t2)
+        {
+            if (AllowedPour(t1, t2))
+            {
+                int amountToPour = Math.Min(t2.FreeAmount, t1.Capacity - t1.CurrentAmount);
+                t1.AddJuice(amountToPour, t2.Juice);
+                t2.CurrentAmount -= amountToPour;
+                Console.WriteLine("Process is OK!");
+            }
+            else
+            {
+                Console.WriteLine("Process isn't OK!");
+            }
+        }
+    }
+}
---
+++ Tank.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Modul
+{
+    public class Tank
+    {
+        private int capacity;
+        private int currentAmount;
+
+        public int Capacity
+        {
+            get => capacity;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Tank capacity must be a positive value.");
+                }
+                capacity = value;
+            }
+        }
+
+        public int CurrentAmount
+        {
+            get => currentAmount;
+            set
+            {
+                //if (value < 0)
+                //{
+                //    throw new ArgumentException("Current amount cannot be negative.");
+                //}з
+                //if (value > Capacity)
+                //{
+                //    throw new ArgumentException("Current amount cannot exceed the tank's capacity.");
+                //}
+                currentAmount = value;
+            }
+        }
+
+        public string Id { get; set; }
+        public JuiceType Juice { get; set; }
+
+        public int FreeAmount => Capacity - CurrentAmount;
+
+        public bool IsFree => CurrentAmount == 0;
+        public bool IsFull => CurrentAmount >= (CriticalPercentage * Capacity) / 100;
+
+        public static int CriticalPercentage { get; set; } = 90;
+        public static int TankCount { get; set; } = 0;
+
+        public Tank(string id, int capacity)
+        {
+            Id = id;
+            Capacity = capacity;
+            CurrentAmount = 0;
+            Juice = JuiceType.Unknown;
+            TankCount++;
+        }
+
+        public Tank(string id, int capacity, int currentAmount, JuiceType juice)
+        {
+            Id = id;
+            Capacity = capacity;
+            CurrentAmount = currentAmount; // Встановлюємо значення через властивість, щоб викликалася перевірка
+            Juice = currentAmount > 0 ? juice : JuiceType.Unknown;
+            TankCount++;
+        }
+
+        public bool AddJuice(int amount, JuiceType juiceType)
+        {
+            if (amount < 0 || (juiceType != Juice && !IsFree))
+            {
+                return false;
+            }
+
+            int newAmount = CurrentAmount + amount;
+
+            if (newAmount > Capacity)
+            {
+                return false;
+            }
+
+            CurrentAmount = newAmount;
+            Juice = juiceType;
+            return true;
+        }
+
+        public void MakeFree()
+        {
+            CurrentAmount = 0;
+            Juice = JuiceType.Unknown;
+        }
+
+        public string GetInfo()
+        {
+            return $"Id: {Id}, Capacity: {Capacity}, Current amount: {CurrentAmount}, Juice type: {Juice}";
+        }
+
+        public static bool ChangeCriticalPercentage(int newPercentage)
+        {
+            if (newPercentage >= 0 && newPercentage <= 100)
+            {
+                CriticalPercentage = newPercentage;
+                return true;
+            }
+            return false;
+        }
+    }
+}
